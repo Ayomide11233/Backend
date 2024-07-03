5b0fa388602c96bf4ff3a0b7ffc165c4ae09769a
Ayomide11233 <ayomide.sayoadeyemi@ucalgary.ca>
2024-07-03 05:54:46 -0600

    Understanding the request

--- 3.2 HTTP Request/index.js
+++ 3.2 HTTP Request/index.js
@@ -1,1 +1,22 @@
 import express from "express";
+const app = express();
+const port = 4000;
+
+app.get("/", (req,res) => {
+    res.send("<h1>Home Page</h1>");
+})
+
+app.get("/about", (req,res) => {
+    res.send("<h1>About</h1>");
+})
+
+app.get("/contact", (req,res) => {
+    res.send("<h1>Contact</h1>" + "<br>" + "<p>You can contact me on Insagram @ayo_ady</p>" );
+   
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Your server ${port} is effective`);
+})
